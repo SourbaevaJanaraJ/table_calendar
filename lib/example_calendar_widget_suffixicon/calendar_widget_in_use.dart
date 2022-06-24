f819551886ed60114d7a21afb2cabc580c30dbb2
Janara Sourbaeva <Janara2610@gmail.com>
2022-06-25 00:44:34 +0600

    created 3 ways of adding calendar

---
+++ lib/example_calendar_widget_suffixicon/calendar_widget_in_use.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'calendar_widget.dart';
+
+class UseCalendarWidget extends StatelessWidget {
+  const UseCalendarWidget({super.key, required this.birthDate});
+
+  final TextEditingController birthDate;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Center(
+          child: TextFormField(
+            decoration: InputDecoration(
+              hintText: 'date of birth',
+              suffixIcon: CalendarWidget(
+                controller: birthDate,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
